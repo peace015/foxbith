8d55624f49b20ee8574021191421b125f79edaa0
Panupong P <panupong.pruk@gmail.com>
2021-04-22 22:01:52 +0700

    add usestate

--- src/App.js
+++ src/App.js
@@ -1,55 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import RadioButtons from './RadioDes';
 import TextField from '@material-ui/core/TextField';
 
-export default function App() {
+function App() {
+  const [inputlist, setInputlist] = useState([{ Description: '' }]);
+
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
+    const list = [...inputlist];
+    list[index][name] = value;
+    setInputlist(list);
+  };
+  const handleRemoveClick = (index) => {
+    const list = [...inputlist];
+    list.splice(index, 1);
+    setInputlist(list);
+  };
+
+  const handleAddClick = () => {
+    setInputlist([...inputlist, { Description: '' }]);
+  };
   return (
-    <div>
+    <div className="App">
       <div>
         <div>
-          <Card>
-            <div>
-              <h1 className="headline">Foxbith Questionnaire</h1>
-              <hr />
+          <div>
+            <Card>
+              <div>
+                <h1 className="headline">Foxbith Questionnaire</h1>
+                <hr />
 
-              <CardActions>
-                <Button size="small" color="primary">
-                  Cancel
-                </Button>
-                <Button size="small" color="primary">
-                  Save
-                </Button>
-              </CardActions>
-            </div>
-          </Card>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Cancel
+                  </Button>
+                  <Button size="small" color="primary">
+                    Save
+                  </Button>
+                </CardActions>
+              </div>
+            </Card>
+            <Card>
+              <div>
+                <h2 className="Head">Questionnaire Detail</h2>
+                <TextField required id="standard-required" label="Name" defaultValue="Name"></TextField>
+              </div>
+            </Card>
+            <hr />
+          </div>
+          
+          {inputlist.map((x, i) => {
+            return (
+
           <Card>
-            <div>
-              <h2 className="Head">Questionnaire Detail</h2>
-              <TextField required id="standard-required" label="Name" defaultValue="Name"></TextField>
+            <div className="form-group">
+              <h2 className="questionNumber">Question1</h2>
+              <TextField required id="standard-required" label="Question" defaultValue="Question"></TextField>
             </div>
           </Card>
-          <hr />
+          {inputlist.map((x, i) => {
+            return (
+              <Card>
+                <div>
+                  <RadioButtons>
+                    <input
+                      className="ml10"
+                      Name="Description"
+                      value={x.Description}
+                      onChange={(e) => handleInputChange(e, i)}
+                    />
+                  </RadioButtons>
+                </div>
+                <div className="btn-box">
+                  {inputlist.length !== 1 && (
+                    <button className="mr10" onClick={() => handleRemoveClick(i)}>
+                      Remove
+                    </button>
+                  )}
+                  {inputlist.length - 1 === i && <button onClick={handleAddClick}>Add</button>}
+                </div>
+                <hr />
+              </Card>
+            );
+          })}
+           
+<Card>
+           <div className="btn-box">
+                  {inputlist.length !== 1 && (
+                    <button className="mr10" onClick={() => handleRemoveClick(i)}>
+                      DELETE
+                    </button>
+                  )}
+                  {inputlist.length - 1 === i && <button onClick={handleAddClick}>DUPLICATE</button>}
+                </div>
+                </Card>
+            );
+        })}
+ 
+       
         </div>
-        <Card>
-          <div className="form-group">
-            <h2 className="questionNumber">Question1</h2>
-            <TextField required id="standard-required" label="Question" defaultValue="Question"></TextField>
-            {/* <input type="text" className="form-control" name="question" autocomplete="off" /> */}
-          </div>
-          <div>
-            <RadioButtons></RadioButtons>
-          </div>
-          <div>
-            <RadioButtons></RadioButtons>
-          </div>
-          <h5 className="add choice">Add Choice</h5>
-          <hr />
-          <h5 className="dubplicate,delete">DUBPLICATE,DELETE</h5>
-        </Card>
       </div>
     </div>
   );
 }
+
+export default App;
